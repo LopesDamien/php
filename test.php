f7621f47efbf6f09859eda50e4aa7a5b4021601a
DamienLopes <damienlopes80@gmail.com>
2022-11-16 17:03:40 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$Nombre=array();
+
+$Nombre[0]='0';
+$Nombre[1]='1';
+$Nombre[2]='2';
+$Nombre[3]='3';
+$Nombre[4]='4';
+echo $Nombre[4];
+for ($m=0;$m<count($tab2);$m++){
+    echo $tab2[1][$m];
+}
+?>
+ 
+ 
+   
+   
+</body>
+</html>
