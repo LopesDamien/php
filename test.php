508899911587c95cf03de09bb10c72079c575dc6
DamienLopes <damienlopes80@gmail.com>
2022-11-23 16:37:39 +0100

    debut

--- test.php
+++ test.php
@@ -7,21 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-<?php
 
-$Nombre=array();
-
-$Nombre[0]='0';
-$Nombre[1]='1';
-$Nombre[2]='2';
-$Nombre[3]='3';
-$Nombre[4]='4';
-echo $Nombre[4];
-for ($m=0;$m<count($tab2);$m++){
-    echo $tab2[1][$m];
-}
-?>
+<?php 
  
+
+?>
+
  
    
    
